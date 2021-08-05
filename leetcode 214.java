a0a6af0de19d02da0eeb03541132a926094046a0
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-08-06 01:24:05 +0530

    Create leetcode 214.java

---
+++ leetcode 214.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/shortest-palindrome/
+class Solution {
+    public String shortestPalindrome(String s) {
+        StringBuilder p = new StringBuilder(s); //pp(p.toString());
+        StringBuilder rev = new StringBuilder(s).reverse(); //pp(rev.toString());
+        p.append("$").append(rev);//pp(p.toString());
+        int n = p.length();
+        int []lps = new int[n];
+        int i = 0;
+        for(int j = 1 ; j < n ;){
+            if(p.charAt(i) == p.charAt(j)){
+                lps[j] = i + 1;
+                i++;
+                j++;
+            }else{
+                if(i > 0){
+                    i = lps[i-1];
+                }
+                else{
+                    j++;
+                }
+            }
+        }//pp(""+Arrays.toString(lps));
+        // abad$daba => need to remove aba
+        return rev.substring(0,rev.length()-lps[n-1]) + s;
+    }
+    void pp(String s){
+        System.out.println(s);
+    }
+}
+/*
+"aacecaaa"
+
+aacecaaa$aaacecaa
+01
+
+*/
