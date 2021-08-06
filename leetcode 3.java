9320e4a7af154741c838a3d33b073c704de73413
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-08-06 23:52:14 +0530

    Update leetcode 3.java

--- leetcode 3.java
+++ leetcode 3.java
@@ -2,20 +2,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         if(s.length() <= 1) return s.length();
-        HashSet<Character>  hs = new HashSet();
-        int i = 0, n = s.length(), maxLen = 1;
-        hs.add(s.charAt(i));//[c,a,b]
-        for(int j = 1; j < n ; j++){
+        int []hash = new int[128];// largest index
+        int left = -1, n = s.length(), maxLen = 1;
+        Arrays.fill(hash,-1); 
+        for(int j = 0; j < n ; j++){
             char ch = s.charAt(j);
-            while(hs.contains(ch)){
-                hs.remove(s.charAt(i++));    
+            if(hash[ch] != -1){
+                left = Math.max(left,hash[ch]);
             }
-            hs.add(ch);
-            maxLen = Math.max(maxLen,j-i+1);
+            maxLen = Math.max(maxLen,j-left);    
+            hash[ch] = j;
         }
         return maxLen;
     }
 }
 /*
-abcabcbb
+"abcabcbb"
+"pwwkew"
 */
