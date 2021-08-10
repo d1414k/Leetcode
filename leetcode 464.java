df01eda14140ba2cba059c6d1087c9347ea91942
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-08-11 00:13:32 +0530

    Update leetcode 464.java

--- leetcode 464.java
+++ leetcode 464.java
@@ -1,33 +1,33 @@
 // https://leetcode.com/problems/can-i-win/
 // https://leetcode.com/problems/can-i-win/discuss/95277/Java-solution-using-HashMap-with-detailed-explanation
 class Solution {
-    Map<Integer,Boolean> dp = null;
+    int []dp;
     int maxChoosableInteger,desiredTotal;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        dp = new HashMap();
+        dp = new int[1<<20];
         this.maxChoosableInteger = maxChoosableInteger;
         this.desiredTotal = desiredTotal;
         if(desiredTotal < 0 || (maxChoosableInteger*(maxChoosableInteger+1))/2 < desiredTotal )
             return false;
         if(desiredTotal <= maxChoosableInteger)
             return true;
-        return helper(0,0);
+        return helper(0,0) == 1 ? true : false;
     }
-    // It returns true if A wins
-    boolean helper(int mask,int curSum){
+    // It returns 1 if A wins
+    int helper(int mask,int curSum){
         if(curSum >= desiredTotal)
-             return false;
-        if(dp.containsKey(mask)) return dp.get(mask);
+             return -1;
+        if(dp[mask] != 0) return dp[mask];
         boolean res = false;//p(curSum+"");
-        for(int i = 1 ; i <= maxChoosableInteger ; i++){// for A
+        for(int i = 0 ; i < maxChoosableInteger ; i++){// for A
             // i can choose this no and opponent can't win
-            if((mask &(1<<i))  == 0 && !helper(mask | (1<<i),curSum+i)) { //p(i+" i");
-                dp.put(mask,true);
-                return true;
+            if((mask &(1<<i))  == 0 && helper(mask | (1<<i),curSum+i+1) == -1)  { //p(i+" i");
+                dp[mask] = 1;
+                return 1;
             }
         }
-        dp.put(mask,false);
-        return false;
+        dp[mask] = -1;
+        return -1;
     }
     void p(String s){
         System.out.println(s);
