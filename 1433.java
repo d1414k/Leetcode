8634c24769469804f774c285a06245b9eef6411a
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-09-02 01:57:15 +0530

    Update 1433.java

--- 1433.java
+++ 1433.java
@@ -1,3 +1,36 @@
+// https://leetcode.com/problems/check-if-a-string-can-break-another-string/
+
+class Solution {
+    /*
+    we can create two count array corresponding to each string s1 and s2
+    if(c1[i] < c2[i]) 
+        s1BreakS2 = false;
+    // means there exist a char in s2 for which this no char in s1 such that it satisfy >= (breaking) condition
+    TC : O(n)
+    SC : O(1)
+    */
+    public boolean checkIfCanBreak(String s1, String s2) {
+        int n = s1.length();
+        int []c1 = new int[26];
+        int []c2 = new int[26];
+        for(int i = 0 ; i < n ; i++){
+            c1[s1.charAt(i)-'a']++;
+            c2[s2.charAt(i)-'a']++;
+        }
+        boolean s1BreakS2 = true,s2BreakS1 = true;
+        for(int i = 0 ; i < 26 ; i++){
+            if(i > 0){
+                c1[i] += c1[i-1];
+                c2[i] += c2[i-1];
+            }
+            if(c1[i] < c2[i])
+                s1BreakS2 = false;
+            else if(c1[i] > c2[i])
+                s2BreakS1 = false;
+        }
+        return s1BreakS2 | s2BreakS1;
+    }
+}
 // class Solution {
 //     public boolean checkIfCanBreak(String s1, String s2) {
 //         int n = s1.length();
