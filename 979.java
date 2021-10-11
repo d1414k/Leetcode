0e209e47dba738023914ebb49e5bfe16573075e8
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-10-11 23:30:34 +0530

    Update 979.java

--- 979.java
+++ 979.java
@@ -15,6 +15,7 @@
  *     }
  * }
  */
+
 class Solution {
     /*
     If we know sum of nodes and sum of coins,
@@ -28,20 +29,46 @@ class Solution {
         helper(root);
         return res;
     }
-    
-    int[] helper(TreeNode root) {
+    // return no of excess nodes
+    int helper(TreeNode root) {
         if(root == null) 
-            return new int[2];
-        int []a = new int[2]; // size,coins
-        int []left = helper(root.left);
-        int []right = helper(root.right);
-        a[0] = left[0] + right[0] + 1;
-        a[1] = left[1] + right[1] + root.val;
-        res += Math.abs(a[0]-a[1]);
-        return a;
+            return 0;
+
+        int left = helper(root.left); // We got these many node from LST
+        int right = helper(root.right);// We got these many node from RST
+        
+        res += Math.abs(left) + Math.abs(right);
+        return root.val + left + right - 1;
     }
     
 }
+// class Solution {
+//     /*
+//     If we know sum of nodes and sum of coins,
+//     Its diff will total movement for that edge
+//     */
+//     Map<TreeNode, int[]> map = null;
+//     static int res = 0;
+//     public int distributeCoins(TreeNode root) {
+//         map = new HashMap();
+//         res = 0;
+//         helper(root);
+//         return res;
+//     }
+    
+//     int[] helper(TreeNode root) {
+//         if(root == null) 
+//             return new int[2];
+//         int []a = new int[2]; // size,coins
+//         int []left = helper(root.left);
+//         int []right = helper(root.right);
+//         a[0] = left[0] + right[0] + 1;
+//         a[1] = left[1] + right[1] + root.val;
+//         res += Math.abs(a[0]-a[1]);
+//         return a;
+//     }
+    
+// }
 // class Solution {
 //     /*
 //     If we know sum of nodes and sum of coins,
