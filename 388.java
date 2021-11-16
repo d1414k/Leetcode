b6784d991f78b98790a91d15df86d8d22e8da33b
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-11-16 23:07:59 +0530

    Create 388.java

---
+++ 388.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/longest-absolute-file-path/
+
+class Solution {
+    /*
+    We per diagram and input we can see that when we see \t column will increase
+    so we can save length string at each column
+    when we see a file, we will comput path length
+    */
+    static int []columnLength = new int[10001]; // length of string at column i
+    public int lengthLongestPath(String input) {
+        int n = input.length(), maxLength = 0;
+        String []line = input.split("\n");
+        for(int i = 0 ; i < line.length ; i++) {
+            int index = line[i].lastIndexOf("\t") + 1;
+            int wordLength = line[i].length() - index;
+            columnLength[index] = wordLength;
+            if(line[i].contains(".")){
+                int pathLen = 0;
+                for(int j = 0 ; j <= index ; j++) {
+                    pathLen += columnLength[j] + 1; // 1 for /
+                } 
+                maxLength = Math.max(pathLen - 1, maxLength);
+            }
+        }
+        return maxLength;
+    }
+}
