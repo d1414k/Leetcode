128cd9b61451e97ff17f1fa8edbb85d94d875786
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-11-16 23:49:16 +0530

    Update 388.java

--- 388.java
+++ 388.java
@@ -2,26 +2,43 @@
 
 class Solution {
     /*
-    We per diagram and input we can see that when we see \t column will increase
+    As per diagram and input we can see that when we see \t column will increase
     so we can save length string at each column
     when we see a file, we will comput path length
     */
-    static int []columnLength = new int[10001]; // length of string at column i
+    // static int []columnLength = new int[10001]; // length of string at column i
+    // public int lengthLongestPath(String input) {
+    //     int n = input.length(), maxLength = 0;
+    //     String []line = input.split("\n");
+    //     for(int i = 0 ; i < line.length ; i++) {
+    //         int index = line[i].lastIndexOf("\t") + 1;
+    //         int wordLength = line[i].length() - index;
+    //         columnLength[index] = wordLength;
+    //         if(line[i].contains(".")){
+    //             int pathLen = 0;
+    //             for(int j = 0 ; j <= index ; j++) {
+    //                 pathLen += columnLength[j] + 1; // 1 for /
+    //             } 
+    //             maxLength = Math.max(pathLen - 1, maxLength);
+    //         }
+    //     }
+    //     return maxLength;
+    // }
+    
+    static int []cummulativePathLength = new int[10001]; // length of string at column i
     public int lengthLongestPath(String input) {
         int n = input.length(), maxLength = 0;
         String []line = input.split("\n");
         for(int i = 0 ; i < line.length ; i++) {
             int index = line[i].lastIndexOf("\t") + 1;
-            int wordLength = line[i].length() - index;
-            columnLength[index] = wordLength;
+            int wordLength = line[i].length() - index + 1;// 1 for /
+            cummulativePathLength[index + 1] = wordLength + cummulativePathLength[index];
             if(line[i].contains(".")){
-                int pathLen = 0;
-                for(int j = 0 ; j <= index ; j++) {
-                    pathLen += columnLength[j] + 1; // 1 for /
-                } 
-                maxLength = Math.max(pathLen - 1, maxLength);
+                maxLength = Math.max(cummulativePathLength[index + 1] - 1, maxLength);
             }
         }
         return maxLength;
     }
+    
+    
 }
