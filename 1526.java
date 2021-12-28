6a4e3b22db46f103369554893e2787a245b3b0f7
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2021-12-29 01:02:35 +0530

    Update 1526.java

--- 1526.java
+++ 1526.java
@@ -7,27 +7,43 @@ class Solution {
     TC : O(n)
     SC : O(n)
     */
+    // public int minNumberOperations(int[] target) {
+    //     int ans = 0;
+    //     Deque<Integer> stack = new ArrayDeque(); // non-decreasing stack
+    //     for(int cur : target) {
+    //         while(!stack.isEmpty() && stack.peek() > cur) {
+    //             int top = stack.pop();
+    //             if(stack.isEmpty()) {
+    //                 ans += top - cur;
+    //             }else{
+    //                 ans += top - Math.max(stack.peek(), cur);
+    //             }
+    //         }
+    //         stack.push(cur);
+    //     }
+    //     while(!stack.isEmpty()) {
+    //         int top = stack.pop();
+    //         if(stack.isEmpty()) {
+    //             ans += top;
+    //         }else{
+    //             ans += top - stack.peek();
+    //         }
+    //     }
+    //     return ans;
+    // }
+    /*
+    -we know that at least we need max element #opetaions
+    -if we reach to element a[k] then all the elements <= a[k]
+    to its left or right will also be covered
+    
+    TC : O(n)
+    SC : O(1)
+    */
     public int minNumberOperations(int[] target) {
-        int ans = 0;
-        Deque<Integer> stack = new ArrayDeque(); // non-decreasing stack
-        for(int cur : target) {
-            while(!stack.isEmpty() && stack.peek() > cur) {
-                int top = stack.pop();
-                if(stack.isEmpty()) {
-                    ans += top - cur;
-                }else{
-                    ans += top - Math.max(stack.peek(), cur);
-                }
-            }
-            stack.push(cur);
-        }
-        while(!stack.isEmpty()) {
-            int top = stack.pop();
-            if(stack.isEmpty()) {
-                ans += top;
-            }else{
-                ans += top - stack.peek();
-            }
+        int ans = target[0];
+        for(int i = 1 ; i < target.length ; i++) {
+            if(target[i] > target[i-1])
+                ans += target[i] - target[i-1];
         }
         return ans;
     }
