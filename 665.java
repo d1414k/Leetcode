66f9c46d2fc90a2cb5fe98ab497ad1ebfee98828
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2022-06-26 02:30:09 +0530

    Update 665.java

--- 665.java
+++ 665.java
@@ -1,1 +1,25 @@
+// https://leetcode.com/problems/non-decreasing-array/
 
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        boolean modified = false;
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i-1] > nums[i]){
+                if(modified) {
+                    return false;
+                }
+                modified = true;
+                // can modify i-1
+                // if(i == 1)
+                //     nums[i-1] = Integer.MIN_VALUE;
+                // else if(nums[i-2] <= nums[i])
+                //     nums[i-1] = nums[i-2];
+                // else // need to modify i
+                //     nums[i] = nums[i-1];
+                if(i > 1 && nums[i-2] > nums[i])
+                    nums[i] = nums[i-1];
+            }
+        }
+        return true;
+    }
+}
