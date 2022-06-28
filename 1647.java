f4db1c7624734232a4b9f7d37fdf3aec448c8536
Deepak Kumar Vishwakarma <30447326+d1414k@users.noreply.github.com>
2022-06-29 01:37:15 +0530

    Update 1647.java

--- 1647.java
+++ 1647.java
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
 
 class Solution {
+    public int minDeletions(String s) {
+        int []frequency = new int[26];
+        for(int i = 0 ; i < s.length() ; i++) {
+            frequency[s.charAt(i)-'a']++;
+        }
+        Arrays.sort(frequency);
+        int maxAvailableNo = s.length(), res = 0;
+        for(int i = 25 ; i >= 0 && frequency[i] > 0 ; i--) {
+            if(frequency[i] > maxAvailableNo) {
+                res += frequency[i] - maxAvailableNo;
+                if(maxAvailableNo > 0) // Why 0 eg. "bbcebab"
+                    maxAvailableNo--;
+            }
+            else 
+                maxAvailableNo = frequency[i] - 1; 
+        }
+        return res;
+    }
+}
+
+
+class Solution {
     // we can process count array in non-increasing order to get empty cell quickly
     public int minDeletions(String s) {
         boolean []map = new boolean[100001];
